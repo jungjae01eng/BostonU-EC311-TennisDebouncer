6f4127a1af14ab638ec54c8663cbca47e7fc4c4e
Jungjae <100228417+jungjae01eng@users.noreply.github.com>
2023-04-25 19:30:29 -0400

    Add files via upload

--- tennis_ball.v
+++ tennis_ball.v
@@ -30,10 +30,19 @@ module tennis_ball(
         output reg [15:0] ball
     );
     
-    
+    Debouncer left_button(.clk(newclock), .PB(left_trigger), .PB_state(PB_left), .PB_down(PB_downleft), .PB_up(PB_upleft), .reset(reset));
+    Debouncer right_button(.clk(newclock), .PB(right_trigger), .PB_state(PB_right), .PB_down(PB_downright), .PB_up(PB_upright), .reset(reset));
     clk_divider cd(.clk_in(clk), .rst(clkreset), .toggle_value(toggle_val), .divided_clk(newclock));
     
-    //reg [4:0] location;
+    wire PB_left;  
+    wire PB_downleft;  
+    wire PB_upleft;
+    
+    wire PB_right;  
+    wire PB_downright;  
+    wire PB_upright;
+    
+    
     reg direction; // 1 = left direction; 0 = right direction;
     wire newclock;
     reg game_off;
@@ -43,38 +52,7 @@ module tennis_ball(
     reg [1:0] right_win;
     reg [1:0] left_win;
     reg [21:0] toggle_val = 22'b1101011110000100000000;
-    
-//    always@ (posedge newclock or posedge reset)
-//        begin
-//            if(reset == 1)
-//                begin
-//                    ball = 16'b0000000000000001;
-//                    game_off = 1;
-//                    direction = 1;
-//                    player = 1;
-//                    right_miss = 0;
-//                    left_miss = 0;  
-//                    right_win = 0;
-//                    left_win = 0;
-//                end
-//            else
-//                begin
-//                    if(direction == 1)
-//                        begin
-//                            if(right_trigger)
-//                                game_off = 0;
-//                        end
-//                    else if(direction == 0)
-//                        begin
-//                            if(left_trigger)
-//                                game_off = 0;
-//                        end
-                                        
-//                end   
-//        end
-    
-    
-    
+       
     
     always@ (posedge newclock or posedge reset)
             begin
@@ -98,61 +76,94 @@ module tennis_ball(
                                     begin
                                         if(ball == 16'b1000000000000000)
                                             begin
-                                            if(left_trigger == 0)
+                                            if(PB_left == 0)
                                                 begin
                                                     player = ~player;
                                                     game_off = 1;
                                                     right_win = right_win + 1;
                                                 end
-                
+                                            right_miss = 0;
+                                            left_miss = 0;
                                             direction = 0;
-                                            toggle_val = toggle_val - 22'b1111111111111;
+                                            toggle_val = toggle_val - 22'b11111111111111111;
                                             end
-                                        else
-//                                            if(left_trigger == 1)
-//                                                begin
-//                                                    left_miss = left_miss + 1;
-//                                                end                       
-                                            ball = ball << 1;
+                                        else               
+                                        //set this to be the cpu for squash                                                    
+                                                ball = ball << 1;
+                                                if(PB_left == 1)
+                                                        left_miss = left_miss + PB_left; 
+                                                if(left_miss == 3)
+                                                    begin
+                                                        player = ~player;
+                                                        game_off = 1;
+                                                        right_win = right_win + 1;
+                                                        ball = 16'b0000000000000001;
+                                                        direction = 1'b0;    
+                                                    end           
+                                                
                                     end
                                 else 
                                     begin
                                         if(ball == 16'b0000000000000001)
                                             begin
-                                            if(right_trigger == 0)
+                                            if(PB_right == 0)
                                                 begin
                                                     player = ~player;
                                                     game_off = 1;
                                                     left_win = left_win + 1;
                                                 end
                                                 direction = 1'b1; 
-                                                toggle_val = toggle_val - 22'b1111111111111;
+                                                right_miss = 0;
+                                                left_miss = 0;
+                                                toggle_val = toggle_val - 22'b11111111111111111;
                                           end
-                                       else       
-//                                            if(right_trigger == 1)
-//                                                begin
-//                                                    right_miss = right_miss + 1;
-//                                                end                        
-                                            ball = ball >> 1;
+                                       else                                                                     
+                                                
+                                                ball = ball >> 1;
+                                                if(PB_right == 1)
+                                                        right_miss = right_miss + PB_right;
+                                                if(right_miss == 3)
+                                                    begin
+                                                        player = ~player;
+                                                        game_off = 1;
+                                                        left_win = left_win + 1;
+                                                        ball = 16'b1000000000000000;
+                                                        direction = 1'b1;
+                                                    end
+                                                
                                   end
                             end                         
                         else   
                             begin
                                 if(right_win == 3)
-                                    ball = 16'b0000100000000001;
+                                    begin
+                                        ball = 16'b0000100000000001;
+                                        left_win = 0;
+                                        right_win = 0;
+                                    end
                                 else if(left_win == 3)
-                                    ball = 16'b0100000000010000;
+                                    begin
+                                        ball = 16'b0100000000010000;
+                                        left_win = 0;
+                                        right_win = 0;
+                                    end
                                 else
-                                    toggle_val = 22'b1101011110000100000000;
+                                    //toggle_val = 22'b1101011110000100000000;
                                     if(direction == 1)
                                         begin
-                                            if(right_trigger == 1)
-                                            game_off = 0;
+                                            if(PB_right == 1)
+                                                begin
+                                                    game_off = 0;
+                                                    toggle_val = 22'b1101011110000100000000;
+                                                end
                                         end
                                     else if (direction == 0)
                                         begin
-                                            if(left_trigger == 1)
-                                            game_off = 0;
+                                            if(PB_left == 1)
+                                                begin
+                                                    game_off = 0;
+                                                    toggle_val = 22'b1101011110000100000000;
+                                                end
                                         end
                                                             
                            end   
