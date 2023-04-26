ae51916ba94e34dd38095f337c95ec79f3df7b63
Jungjae <100228417+jungjae01eng@users.noreply.github.com>
2023-04-26 19:35:22 -0400

    Add files via upload

--- Debouncer.v
+++ Debouncer.v
@@ -30,33 +30,39 @@ module Debouncer(
     output PB_up   // 1 for one clock cycle when the push-button goes up (i.e. just released)
 );
 
-// First use two flip-flops to synchronize the PB signal the "clk" clock domain
-reg PB_sync_0;  always @(posedge clk) PB_sync_0 <= ~PB;  // invert PB to make PB_sync_0 active high
-reg PB_sync_1;  always @(posedge clk) PB_sync_1 <= PB_sync_0;
+    // First use two flip-flops to synchronize the PB signal the "clk" clock domain
+    reg PB_sync_0;  always @(posedge clk) PB_sync_0 <= ~PB;  // invert PB to make PB_sync_0 active high
+    reg PB_sync_1;  always @(posedge clk) PB_sync_1 <= PB_sync_0;
+    
+    // Next declare a 16-bits counter
+    reg [20:0] PB_cnt;
+    
+    // When the push-button is pushed or released, we increment the counter
+    // The counter has to be maxed out before we decide that the push-button state has changed
+    
+    wire PB_idle = (PB_state==PB_sync_1);
+    wire PB_cnt_max = &PB_cnt;	// true when all bits of PB_cnt are 1's
+    
+    always@ (posedge clk) begin
+        if (reset) begin
+            PB_state <= 0;
+        end
+        else if (PB_idle) begin
+            PB_cnt <= 0;  // nothing's going on
+        end
+        else begin
+            PB_state <= PB;
+            PB_cnt <= PB_cnt + 21'd1;  // something's going on, increment the counter
+            if (PB_cnt_max) begin
+                PB_state <= ~PB_state;  // if the counter is maxed out, PB changed!
+            end
+            if (~PB_state) begin
+                PB_cnt <=0;
+            end
+        end
+    end
+    
+    assign PB_down = ~PB_idle & PB_cnt_max & ~PB_state;
+    assign PB_up   = ~PB_idle & PB_cnt_max &  PB_state;
 
-// Next declare a 16-bits counter
-reg [20:0] PB_cnt;
-
-// When the push-button is pushed or released, we increment the counter
-// The counter has to be maxed out before we decide that the push-button state has changed
-
-wire PB_idle = (PB_state==PB_sync_1);
-wire PB_cnt_max = &PB_cnt;	// true when all bits of PB_cnt are 1's
-
-always @(posedge clk)
-if(reset)
-    PB_state <= 0;
-else if(PB_idle)
-    PB_cnt <= 0;  // nothing's going on
-else
-begin
-    PB_state <= PB;
-    PB_cnt <= PB_cnt + 21'd1;  // something's going on, increment the counter
-    if(PB_cnt_max) PB_state <= ~PB_state;  // if the counter is maxed out, PB changed!
-    if(~PB_state)
-        PB_cnt <=0;
-end
-
-assign PB_down = ~PB_idle & PB_cnt_max & ~PB_state;
-assign PB_up   = ~PB_idle & PB_cnt_max &  PB_state;
 endmodule
--- clk_divider.v
+++ clk_divider.v
@@ -16,31 +16,22 @@ module clk_divider(
 	output reg divided_clk
     );
 	 
-//parameter toggle_value = 22'b1101011110000100000000;
-
-	 
-reg[21:0] cnt;
-
-always@(posedge clk_in or posedge rst)
-begin
-	if (rst==1) begin
-		cnt <= 0;
-		divided_clk <= 0;
-	end
-	else begin
-		if (cnt==toggle_value) begin
-			cnt <= 0;
-			divided_clk <= ~divided_clk;
-		end
-		else begin
-			cnt <= cnt +1;
-			divided_clk <= divided_clk;		
-		end
-	end
-
-end
-			  
-	
-
-
+    reg[21:0] cnt;
+    
+    always@ (posedge clk_in or posedge rst) begin
+        if (rst==1) begin
+            cnt <= 0;
+            divided_clk <= 0;
+        end
+        else begin
+            if (cnt==toggle_value) begin
+                cnt <= 0;
+                divided_clk <= ~divided_clk;
+            end
+            else begin
+                cnt <= cnt +1;
+                divided_clk <= divided_clk;		
+            end
+        end
+    end
 endmodule
